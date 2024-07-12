98957503d7e20baff92b2010db9b7f1ad155d290
tracer-ysyx <tracer@ysyx.org>
2024-07-12 19:19:55 +0800

    > compile NEMU ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:19:55 up 31 min,  1 user,  load average: 0.66, 0.57, 0.41

---
+++ npc/obj_dir/Vtop___024root__DepSet_heccd7ead__0.cpp
@@ -0,0 +1,154 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: Verilator output: Design implementation internals
+// See Vtop.h for the primary calling header
+
+#include "Vtop__pch.h"
+#include "Vtop___024root.h"
+
+void Vtop___024root___eval_act(Vtop___024root* vlSelf) {
+    (void)vlSelf;  // Prevent unused variable warning
+    Vtop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vtop___024root___eval_act\n"); );
+}
+
+void Vtop___024root___nba_sequent__TOP__0(Vtop___024root* vlSelf);
+
+void Vtop___024root___eval_nba(Vtop___024root* vlSelf) {
+    (void)vlSelf;  // Prevent unused variable warning
+    Vtop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vtop___024root___eval_nba\n"); );
+    // Body
+    if ((1ULL & vlSelf->__VnbaTriggered.word(0U))) {
+        Vtop___024root___nba_sequent__TOP__0(vlSelf);
+    }
+}
+
+VL_INLINE_OPT void Vtop___024root___nba_sequent__TOP__0(Vtop___024root* vlSelf) {
+    (void)vlSelf;  // Prevent unused variable warning
+    Vtop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vtop___024root___nba_sequent__TOP__0\n"); );
+    // Init
+    SData/*15:0*/ __Vdly__led;
+    __Vdly__led = 0;
+    IData/*31:0*/ __Vdly__top__DOT__count;
+    __Vdly__top__DOT__count = 0;
+    // Body
+    __Vdly__top__DOT__count = vlSelf->top__DOT__count;
+    __Vdly__led = vlSelf->led;
+    if (vlSelf->rst) {
+        __Vdly__led = 1U;
+        __Vdly__top__DOT__count = 0U;
+    } else if (((IData)(vlSelf->a) & (IData)(vlSelf->b))) {
+        if ((0U == vlSelf->top__DOT__count)) {
+            __Vdly__led = ((0xfffeU & ((IData)(vlSelf->led) 
+                                       << 1U)) | (1U 
+                                                  & ((IData)(vlSelf->led) 
+                                                     >> 0xfU)));
+        }
+        __Vdly__top__DOT__count = ((0x3e8U <= vlSelf->top__DOT__count)
+                                    ? 0U : ((IData)(1U) 
+                                            + vlSelf->top__DOT__count));
+    }
+    vlSelf->led = __Vdly__led;
+    vlSelf->top__DOT__count = __Vdly__top__DOT__count;
+}
+
+void Vtop___024root___eval_triggers__act(Vtop___024root* vlSelf);
+
+bool Vtop___024root___eval_phase__act(Vtop___024root* vlSelf) {
+    (void)vlSelf;  // Prevent unused variable warning
+    Vtop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vtop___024root___eval_phase__act\n"); );
+    // Init
+    VlTriggerVec<1> __VpreTriggered;
+    CData/*0:0*/ __VactExecute;
+    // Body
+    Vtop___024root___eval_triggers__act(vlSelf);
+    __VactExecute = vlSelf->__VactTriggered.any();
+    if (__VactExecute) {
+        __VpreTriggered.andNot(vlSelf->__VactTriggered, vlSelf->__VnbaTriggered);
+        vlSelf->__VnbaTriggered.thisOr(vlSelf->__VactTriggered);
+        Vtop___024root___eval_act(vlSelf);
+    }
+    return (__VactExecute);
+}
+
+bool Vtop___024root___eval_phase__nba(Vtop___024root* vlSelf) {
+    (void)vlSelf;  // Prevent unused variable warning
+    Vtop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vtop___024root___eval_phase__nba\n"); );
+    // Init
+    CData/*0:0*/ __VnbaExecute;
+    // Body
+    __VnbaExecute = vlSelf->__VnbaTriggered.any();
+    if (__VnbaExecute) {
+        Vtop___024root___eval_nba(vlSelf);
+        vlSelf->__VnbaTriggered.clear();
+    }
+    return (__VnbaExecute);
+}
+
+#ifdef VL_DEBUG
+VL_ATTR_COLD void Vtop___024root___dump_triggers__nba(Vtop___024root* vlSelf);
+#endif  // VL_DEBUG
+#ifdef VL_DEBUG
+VL_ATTR_COLD void Vtop___024root___dump_triggers__act(Vtop___024root* vlSelf);
+#endif  // VL_DEBUG
+
+void Vtop___024root___eval(Vtop___024root* vlSelf) {
+    (void)vlSelf;  // Prevent unused variable warning
+    Vtop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vtop___024root___eval\n"); );
+    // Init
+    IData/*31:0*/ __VnbaIterCount;
+    CData/*0:0*/ __VnbaContinue;
+    // Body
+    __VnbaIterCount = 0U;
+    __VnbaContinue = 1U;
+    while (__VnbaContinue) {
+        if (VL_UNLIKELY((0x64U < __VnbaIterCount))) {
+#ifdef VL_DEBUG
+            Vtop___024root___dump_triggers__nba(vlSelf);
+#endif
+            VL_FATAL_MT("/home/wangbaosen/ysyx/ysyx-workbench/npc/vsrc/top.v", 1, "", "NBA region did not converge.");
+        }
+        __VnbaIterCount = ((IData)(1U) + __VnbaIterCount);
+        __VnbaContinue = 0U;
+        vlSelf->__VactIterCount = 0U;
+        vlSelf->__VactContinue = 1U;
+        while (vlSelf->__VactContinue) {
+            if (VL_UNLIKELY((0x64U < vlSelf->__VactIterCount))) {
+#ifdef VL_DEBUG
+                Vtop___024root___dump_triggers__act(vlSelf);
+#endif
+                VL_FATAL_MT("/home/wangbaosen/ysyx/ysyx-workbench/npc/vsrc/top.v", 1, "", "Active region did not converge.");
+            }
+            vlSelf->__VactIterCount = ((IData)(1U) 
+                                       + vlSelf->__VactIterCount);
+            vlSelf->__VactContinue = 0U;
+            if (Vtop___024root___eval_phase__act(vlSelf)) {
+                vlSelf->__VactContinue = 1U;
+            }
+        }
+        if (Vtop___024root___eval_phase__nba(vlSelf)) {
+            __VnbaContinue = 1U;
+        }
+    }
+}
+
+#ifdef VL_DEBUG
+void Vtop___024root___eval_debug_assertions(Vtop___024root* vlSelf) {
+    (void)vlSelf;  // Prevent unused variable warning
+    Vtop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vtop___024root___eval_debug_assertions\n"); );
+    // Body
+    if (VL_UNLIKELY((vlSelf->clk & 0xfeU))) {
+        Verilated::overWidthError("clk");}
+    if (VL_UNLIKELY((vlSelf->rst & 0xfeU))) {
+        Verilated::overWidthError("rst");}
+    if (VL_UNLIKELY((vlSelf->a & 0xfeU))) {
+        Verilated::overWidthError("a");}
+    if (VL_UNLIKELY((vlSelf->b & 0xfeU))) {
+        Verilated::overWidthError("b");}
+}
+#endif  // VL_DEBUG
