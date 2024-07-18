9dc8ff1e140ac705190fe951afaf9e0c7565dbe3
tracer-ysyx <tracer@ysyx.org>
2024-07-18 22:40:58 +0800

    > compile NEMU ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:40:58 up  4:45,  1 user,  load average: 1.98, 0.95, 0.66

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -43,9 +43,10 @@ word_t isa_reg_str2val(const char *s, bool *success) {
 
     for(int i=0;i<32;i++)
     {   
-        if(strcmp(regs[i],s)==0)
-        {   printf("gpr:%x,%d",gpr(i)+1,gpr(i)+1);
-            return gpr(i);
+        if(strcmp(regs[i],s)==0){
+            uint32_t temp = gpr(i);
+            printf("gpr:%x,%d",temp,temp);
+            return temp;
         }
     }
     printf("not find the register.\n");
