97cd7267ac10425adf427f975ac9aeb8a488f920
tracer-ysyx <tracer@ysyx.org>
2024-07-18 18:14:40 +0800

    > compile NEMU ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:14:40 up  1:50,  1 user,  load average: 0.29, 0.98, 0.99

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -38,7 +38,7 @@ word_t isa_reg_str2val(const char *s, bool *success) {
     int idx=99;
     for(i=0;i<32;i++)
     {
-        if(strcmp(s,regs[i])==0)
+        if(strcmp(s,reg_name(i))==0)
         {
             idx=i;
         }
