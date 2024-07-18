1af5d6867e5451bfbdb8b236e6d9e96248b0daa4
tracer-ysyx <tracer@ysyx.org>
2024-07-19 02:22:55 +0800

    > compile NEMU ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  02:22:55 up  1:41,  1 user,  load average: 0.85, 0.86, 0.76

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -46,6 +46,10 @@ word_t isa_reg_str2val(const char *s, bool *success) {
             
             return gpr(i);
         }
+        else if(strcmp("pc",s)==0){
+
+            return cpu.pc;
+        }
     }
     printf("not find the register.\n");
     return 0;
--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -265,38 +265,38 @@ int pre_process()
     }
     
 
-    // for(int i = 0 ; i < tokens_len ; i ++)
-    // {
-	// if(
-    // (tokens[i].type == SUB && i > 0 &&
-    // (tokens[i-1].type != NUM || tokens[i-1].type != HEX ||tokens[i-1].type != REGISTER ) &&
-    // (tokens[i+1].type == NUM || tokens[i+1].type == HEX ||tokens[i+1].type == REGISTER ))
-	// 	||
-	// 	(tokens[i].type == SUB && i == 0)
-	//   )
-	// {
+    for(int i = 0 ; i < tokens_len ; i ++)
+    {
+	if(
+    (tokens[i].type == SUB && i > 0 &&\
+    (tokens[i-1].type != NUM || tokens[i-1].type != HEX ||tokens[i-1].type != REGISTER ) &&\
+    (tokens[i+1].type == NUM || tokens[i+1].type == HEX ||tokens[i+1].type == REGISTER ))
+		||
+		(tokens[i].type == SUB && i == 0)
+	  )
+	{
 	    
-	//     tokens[i].type = TK_NOTYPE;
+	    tokens[i].type = TK_NOTYPE;
 	    
-	//     for(int j = 31 ; j >= 0 ; j --)
-    //     {
-	// 	    tokens[i+1].str[j] = tokens[i+1].str[j-1];
-	//     }
-	//     tokens[i+1].str[0] = '-';
+	    for(int j = 31 ; j >= 0 ; j --)
+        {
+		    tokens[i+1].str[j] = tokens[i+1].str[j-1];
+	    }
+	    tokens[i+1].str[0] = '-';
 	    
-	//     for(int j = 0 ; j < tokens_len ; j ++)
-    //     {
-	// 	    if(tokens[j].type == TK_NOTYPE)
-	// 	    {
-	// 	        for(int k = j +1 ; k < tokens_len ; k ++)
-    //             {
-	// 		        tokens[k - 1] = tokens[k];
-	// 	        }
-	// 	        tokens_len -- ;
-	// 	    }
-	//     }
-	// }
-    // }
+	    for(int j = 0 ; j < tokens_len ; j ++)
+        {
+		    if(tokens[j].type == TK_NOTYPE)
+		    {
+		        for(int k = j +1 ; k < tokens_len ; k ++)
+                {
+			        tokens[k - 1] = tokens[k];
+		        }
+		        tokens_len -- ;
+		    }
+	    }
+	}
+    }
 
     
     for(int i = 0 ; i < tokens_len ; i ++)
@@ -493,7 +493,6 @@ uint32_t eval(int p, int q)
                 op = max(op, i);
             }
             if(!flag && tokens[i].type == SUB){
-                printf("sub\n");
                 flag = true;
                 op = max(op, i);
             }
