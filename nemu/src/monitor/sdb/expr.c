d377dfb27202780a6b7cbdac045588a87bb98837
tracer-ysyx <tracer@ysyx.org>
2024-07-19 15:45:25 +0800

    > compile NEMU ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:45:25 up  1:11,  1 user,  load average: 0.33, 0.49, 0.52

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -342,7 +342,7 @@ int pre_process()
             tokens[i].type = TK_NOTYPE;
             uint32_t tmp = char2int(tokens[i+1].str);
             
-            printf("tmp=%lu\n",(long)tmp);
+            printf("tmp=%lx\n",(long)tmp);
 
             uint32_t value = *((uint32_t *)(long)tmp);
             printf("value=%u\n",value);
