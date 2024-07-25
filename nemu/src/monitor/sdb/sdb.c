29ced692c6d2b337b05f8e329d3ee54cbcf50364
tracer-ysyx <tracer@ysyx.org>
2024-07-25 23:41:56 +0800

    > compile NEMU ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:41:56 up  1:29,  1 user,  load average: 1.17, 1.01, 0.88

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -43,6 +43,7 @@ void test_expr() {
     e[read-1] = '\0';
 
     word_t res = expr(e, &success);
+    printf("expected: %u, got: %u\n", correct_res, res);
 
     assert(success);
     if (res != correct_res) {
