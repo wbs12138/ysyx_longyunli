effc92f4adde4da72098f5ef137c39bef3473721
tracer-ysyx <tracer@ysyx.org>
2024-07-26 01:39:51 +0800

    > compile NEMU ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:39:51 up  3:26,  1 user,  load average: 1.11, 1.14, 1.10

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -132,7 +132,7 @@ bool check_parentheses(int p, int q)
             if (tokens[i].type== LPARE ) par++;
             else if (tokens[i].type== RPARE ) par--;
 
-            printf("i=%d,par=%d\n",i,par);
+            printf("p=%d,q=%d,i=%d,par=%d\n",p,q,i,par);
             if (par == 0)
             {
                 if (i==q)
