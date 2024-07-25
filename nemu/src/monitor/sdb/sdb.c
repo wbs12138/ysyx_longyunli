3db56e01738693493c80857ff003410047847d75
tracer-ysyx <tracer@ysyx.org>
2024-07-25 23:36:52 +0800

    > compile NEMU ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:36:52 up  1:23,  1 user,  load average: 1.14, 0.98, 0.84

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -25,6 +25,37 @@ static int is_batch_mode = false;
 void init_regex();
 void init_wp_pool();
 
+void test_expr() {
+  FILE *fp = fopen("/home/ubuntu/ics2022/nemu/tools/gen-expr/build/input", "r");
+  if (fp == NULL) perror("test_expr error");
+
+  char *e = NULL;
+  word_t correct_res;
+  size_t len = 0;
+  ssize_t read;
+  bool success = false;
+
+  while (true) {
+    if(fscanf(fp, "%u ", &correct_res) == -1) break;
+    read = getline(&e, &len, fp);
+    e[read-1] = '\0';
+
+    word_t res = expr(e, &success);
+
+    assert(success);
+    if (res != correct_res) {
+      puts(e);
+      printf("expected: %u, got: %u\n", correct_res, res);
+      assert(0);
+    }
+  }
+
+  fclose(fp);
+  if (e) free(e);
+
+  Log("expr test pass");
+}
+
 /* We use the `readline' library to provide more flexibility to read from stdin. */
 static char* rl_gets() {
   static char *line_read = NULL;
@@ -190,6 +221,8 @@ void sdb_mainloop() {
 void init_sdb() {
   /* Compile the regular expressions. */
   init_regex();
+
+  test_expr();
 
   /* Initialize the watchpoint pool. */
   init_wp_pool();
--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -30,40 +30,94 @@ static char *code_format =
 "  printf(\"%%u\", result); "
 "  return 0; "
 "}";
+int index_gen = 0;
 
-static void gen_rand_expr() {
-  buf[0] = '\0';
+int choose(int n){
+  int a=rand()%3;
+  return a;
 }
 
+void gen_num(){
+  int num_width=rand()%4+1;
+  int num_width_r=num_width;
+  int num_rand;
+  char c;
+  while(num_width){
+    if(num_width==num_width_r)
+      num_rand=rand()%9+1;
+    else
+      num_rand=rand()%10;
+    num_width--;
+    c = num_rand + '0';
+    buf[index_gen ++] = c;
+  }  
+    
+}
+void gen(char c){
+    buf[index_gen ++] = c;
+}
+void gen_rand_op(){
+    char op[4] = {'+', '-', '*', '/'};
+    int op_position = rand() % 4;
+    buf[index_gen ++] = op[op_position];
+}
+
+
+
+static void gen_rand_expr() {
+    //    buf[0] = '\0';	
+   if(index_gen > 65530)
+       	printf("overSize\n");
+       int  rand_num=choose(3);
+    switch (rand_num) {
+	case 0:
+	    gen_num();
+	    break;
+	case 1:
+	    gen('(');
+	    gen_rand_expr();
+	    gen(')');
+	    break;
+	default:
+	    gen_rand_expr();
+	    gen_rand_op();
+	    gen_rand_expr();
+	    break;
+    }
+}
+
+
+
 int main(int argc, char *argv[]) {
-  int seed = time(0);
-  srand(seed);
-  int loop = 1;
-  if (argc > 1) {
-    sscanf(argv[1], "%d", &loop);
-  }
-  int i;
-  for (i = 0; i < loop; i ++) {
-    gen_rand_expr();
+    int seed = time(0);
+    srand(seed);
+    int loop = 1;
+    if (argc > 1) {
+	sscanf(argv[1], "%d", &loop);
+    }
+    int i;
+    for (i = 0; i < loop; i ++) {
+	gen_rand_expr();
+	buf[index_gen] = '\0';
+	sprintf(code_buf, code_format, buf);
 
-    sprintf(code_buf, code_format, buf);
+	FILE *fp = fopen("/tmp/.code.c", "w");
+	assert(fp != NULL);
+	fputs(code_buf, fp);
+	fclose(fp);
 
-    FILE *fp = fopen("/tmp/.code.c", "w");
-    assert(fp != NULL);
-    fputs(code_buf, fp);
-    fclose(fp);
+	int ret = system("gcc /tmp/.code.c -o /tmp/.expr");
+	if (ret != 0) continue;
 
-    int ret = system("gcc /tmp/.code.c -o /tmp/.expr");
-    if (ret != 0) continue;
+	fp = popen("/tmp/.expr", "r");
+	assert(fp != NULL);
 
-    fp = popen("/tmp/.expr", "r");
-    assert(fp != NULL);
+	int result;
+	ret = fscanf(fp, "%d", &result);
+	pclose(fp);
 
-    int result;
-    ret = fscanf(fp, "%d", &result);
-    pclose(fp);
-
-    printf("%u %s\n", result, buf);
-  }
-  return 0;
+	printf("%u %s\n", result, buf);
+	index_gen = 0;
+    }
+    return 0;
 }
