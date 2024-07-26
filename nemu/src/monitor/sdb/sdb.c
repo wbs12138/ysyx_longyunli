afb2be090104ed6dfbb0b5a8a36b09a9cb195e42
tracer-ysyx <tracer@ysyx.org>
2024-07-26 13:36:37 +0800

    > compile NEMU ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:36:37 up 20 min,  1 user,  load average: 0.72, 0.76, 0.62

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -223,9 +223,9 @@ void sdb_mainloop() {
 void init_sdb() {
   /* Compile the regular expressions. */
   init_regex();
-int testnumex=(135  *(0-14));//  -(4-(((32)*(0-(12-69+((0 -66  )/33  )+9506)/76/69*(32)/46  +
+uint32_t testnumex=(135  *(0-14));//  -(4-(((32)*(0-(12-69+((0 -66  )/33  )+9506)/76/69*(32)/46  +
 //71  )*90+55  )))*(0 -85 )/95   *(15-62-10);
-printf("testnumex=%d",testnumex); 
+printf("testnumex=%u",testnumex); 
   //test_expr();
 
   /* Initialize the watchpoint pool. */
