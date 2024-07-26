9d4810c5f9103fc5a2f30d78c581ac694d5f6e1e
tracer-ysyx <tracer@ysyx.org>
2024-07-26 11:03:35 +0800

    > compile NEMU ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:03:35 up 24 min,  1 user,  load average: 0.59, 0.52, 0.55

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -106,7 +106,7 @@ int main(int argc, char *argv[]) {
 	fputs(code_buf, fp);
 	fclose(fp);
 
-	int ret = system("gcc /tmp/.code.c -o /tmp/.expr");
+	int ret = system("gcc /tmp/.code.c -Wall -Werror -o /tmp/.expr");
 	if (ret != 0) continue;
 
 	fp = popen("/tmp/.expr", "r");
