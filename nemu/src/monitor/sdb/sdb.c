b647930692365350e3f86db2637ea47e1474bffa
tracer-ysyx <tracer@ysyx.org>
2024-07-26 13:31:24 +0800

    > compile NEMU ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:31:24 up 14 min,  1 user,  load average: 0.49, 0.66, 0.54

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -223,7 +223,7 @@ void sdb_mainloop() {
 void init_sdb() {
   /* Compile the regular expressions. */
   init_regex();
-int testnumex=6-189 -435  +1421+((175   -40  *(44-58)))/7/6/116  -(4-(((32)*(0-(12-69+((0 -66  )/33  )+9506)/76/69*(32)/46  +
+int testnumex=((175   -40  *(44-58)))/7/6/116  -(4-(((32)*(0-(12-69+((0 -66  )/33  )+9506)/76/69*(32)/46  +
 71  )*90+55  )))*(77 /85   -85   )/95   *(15-62-10);
 printf("testnumex=%d",testnumex); 
   //test_expr();
