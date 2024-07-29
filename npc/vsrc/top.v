802ce4c534e7b1936488b047d553940f33af7245
tracer-ysyx <tracer@ysyx.org>
2024-07-29 21:55:33 +0800

    > sim RTL ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:55:33 up  1:08,  1 user,  load average: 1.48, 1.41, 1.31

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -1,9 +1,10 @@
 module top(
-  input [1:0] Y,X0,X1,X2,X3,
-  output [1:0] F
+  input [1:0] Y,X0,X1,X2,X3,clk,
+  input rst,
+  output reg[1:0] F1
   
 );
-  
+  wire [1:0] F;
   MuxKey #(4, 2, 2) inst_muxkey (F, Y, {
     2'b00, X0,
     2'b01, X1,
@@ -11,4 +12,12 @@ module top(
     2'b11, X3
     
   });
+
+  always@(posedge clk or negedge rst)
+  begin
+    if(!rst)
+    F1<='b0;
+    else
+    F1<=F;
+  end
 endmodule
