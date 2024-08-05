91ac5518ef26e5698017d3a3da62dde83019844c
tracer-ysyx <tracer@ysyx.org>
2024-08-05 22:41:23 +0800

    > sim RTL ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:41:23 up  2:32,  1 user,  load average: 0.97, 0.97, 1.14

--- npc/csrc/main_sim.cpp
+++ npc/csrc/main_sim.cpp
@@ -13,7 +13,7 @@
 
 vluint64_t sim_time=0;
 
-uint8_t pmem[CONFIG_MSIZE] PG_ALIGN = {};
+uint32_t pmem[CONFIG_MSIZE] PG_ALIGN = {};
 
 uint8_t* guest_to_host(uint32_t paddr) { return pmem + paddr - CONFIG_MBASE; }
 
