56a23f98707cfe217fdb8af0f897b8faa05cdd22
tracer-ysyx <tracer@ysyx.org>
2024-08-05 22:51:17 +0800

    > sim RTL ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:51:17 up  2:42,  1 user,  load average: 1.34, 1.23, 1.18

--- npc/csrc/main_sim.cpp
+++ npc/csrc/main_sim.cpp
@@ -28,7 +28,7 @@ uint32_t *iram = NULL ;
 void init_iram() {
 
 
-  iram = malloc(sizeof(uint32_t)*10000);
+  iram = (uint32_t *)malloc(sizeof(uint32_t)*10000);
 	
   memcpy(iram, img, sizeof(img));
   
