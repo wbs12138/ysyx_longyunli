de4053937a79b73cbd4e84e899b5b56caeb2598c
tracer-ysyx <tracer@ysyx.org>
2024-08-05 22:29:56 +0800

    > sim RTL ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:29:56 up  2:21,  1 user,  load average: 1.41, 1.36, 1.41

--- npc/csrc/main_sim.cpp
+++ npc/csrc/main_sim.cpp
@@ -42,7 +42,7 @@ static inline uint32_t host_read(void *addr, int len) {
     case 1: return *(uint8_t  *)addr;
     case 2: return *(uint16_t *)addr;
     case 4: return *(uint32_t *)addr;
-    default: assert(0), return 0;
+    default: assert(0);
   }
 }
 
