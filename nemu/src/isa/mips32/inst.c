4fe4b30a6b8bc5a2fb4d712f2dff27e3ca824065
tracer-ysyx <tracer@ysyx.org>
2024-08-07 08:02:29 +0800

    > compile NEMU ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  08:02:29 up 7 min,  1 user,  load average: 1.47, 1.31, 0.66

--- nemu/src/isa/mips32/inst.c
+++ nemu/src/isa/mips32/inst.c
@@ -70,6 +70,6 @@ static int decode_exec(Decode *s) {
 
 int isa_exec_once(Decode *s) {
   s->isa.inst.val = inst_fetch(&s->snpc, 4);
-  IFDEF(CONFIG_ITRACE, trace_inst(s->pc, s->isa.inst.val));
+  IFDEF(CONFIG_ITRACE, trace_inst(s->pc, s->isa.inst.val),printf("ok\n"););
   return decode_exec(s);
 }
