5e6424cd944df25f1a93b29cceee7adcf61e5583
tracer-ysyx <tracer@ysyx.org>
2024-08-10 00:55:04 +0800

    > compile NEMU ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:55:04 up  3:00,  1 user,  load average: 1.69, 1.69, 1.66

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -60,7 +60,7 @@ static long load_img() {
   Log("The image is %s, size = %ld", img_file, size);
 
   fseek(fp, 0, SEEK_SET);
-  int ret = fread(guest_to_host(RESET_VECTOR), size, 1, fp);
+  int ret = fread(guest_to_host(0x80000000), size, 1, fp);
   assert(ret == 1);
 
   fclose(fp);
