4fbb552bf237d586ec0c2665d9a697d35467c890
tracer-ysyx <tracer@ysyx.org>
2024-08-10 00:11:22 +0800

    > sim RTL ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:11:22 up  2:16,  1 user,  load average: 1.49, 1.26, 1.16

--- npc/csrc/main_sim.cpp
+++ npc/csrc/main_sim.cpp
@@ -1,5 +1,7 @@
 #include <stdlib.h>
 #include <iostream>
+#include <stdio.h>
+#include <assert.h>
 #include <assert.h>
 #include <verilated.h>
 #include <verilated_vcd_c.h>
@@ -7,6 +9,15 @@
 #include "svdpi.h"
 #include "Vtop__Dpi.h"
 
+#define MAX_SIM_TIME 20000
+#define CONFIG_MBASE 0x80000000
+#define CONFIG_MSIZE 0x8000000
+#define CONFIG_PC_RESET_OFFSET 0x0
+#define PMEM_LEFT  ((uint32_t)CONFIG_MBASE)
+#define RESET_VECTOR (PMEM_LEFT + CONFIG_PC_RESET_OFFSET)
+#define PG_ALIGN __attribute((aligned(4096)))
+
+vluint64_t sim_time=0;
 
 int ebreak_dpi=0;
 
@@ -15,51 +26,72 @@ void ebreak(){
 	return;
 }
 
+static uint8_t pmem[CONFIG_MSIZE] PG_ALIGN = {};
 
-#define MAX_SIM_TIME 20000
+uint8_t* guest_to_host(uint32_t paddr) { return pmem + paddr - CONFIG_MBASE; }
+uint32_t host_to_guest(uint8_t *haddr) { return haddr - pmem + CONFIG_MBASE; }
 
-vluint64_t sim_time=0;
+static uint32_t pmem_read(uint32_t addr, int len) {
+  uint32_t ret = host_read(guest_to_host(addr), len);
+  return ret;
+}
 
-static const uint32_t img [] = {
-  0x00300093,  // addi r1 r0 0x03
-  0x00508213,  // addi r4 r1 0x05
-  0x00108293,  // addi r5 r1 0x01
-  0x00128313,  // addi r6 r5 0x01
-  0x00000073,
+static void pmem_write(uint32_t addr, int len, uint32_t data) {
+  host_write(guest_to_host(addr), len, data);
+}
 
-};
+void init_mem() {
+  memset(pmem, rand(), CONFIG_MSIZE);
+  Log("physical memory area [" FMT_PADDR ", " FMT_PADDR "]", PMEM_LEFT, PMEM_RIGHT);
+}
 
+static char *img_file = NULL;
 
-uint32_t *iram = NULL ;
+//static long load_img() 
+void load_img(){
+  if (img_file == NULL) {
+    Log("No image is given. Use the default build-in image.");
+    return ;
+  }
 
+  FILE *fp = fopen(img_file, "rb");
+  Assert(fp, "Can not open '%s'", img_file);
 
-void init_iram() {
+  fseek(fp, 0, SEEK_END);
+  long size = ftell(fp);
 
-  iram = (uint32_t *)malloc(sizeof(uint32_t)*100);
-	
-  memcpy(iram, img, sizeof(img));
-  
+  Log("The image is %s, size = %ld", img_file, size);
+
+  fseek(fp, 0, SEEK_SET);
+  int ret = fread(guest_to_host(RESET_VECTOR), size, 1, fp);
+  assert(ret == 1);
+
+  fclose(fp);
   return ;
 }
 
 
-
-static uint32_t pmem_read(uint32_t addr) {
-  uint32_t idx = (addr - 0x80000000)/4 ;
-  uint32_t ret = iram[idx];
-  return ret;
-}
-
-
-
 int main(int argc,char** argv,char** env){
+	
+	assert(argc==2);
+	
+	int i=1;
+    do{
+		printf("argc[%d]=%s\n",i,argv[i-1]);
+    	i+=1;
+  	}while(i!=argc+1);
+
+	img_file=argv[1];
+
+	load_image();
+
+	init_mem();
+
 	Vtop *dut = new Vtop;
 	Verilated::traceEverOn(true);
 	VerilatedVcdC *m_trace = new VerilatedVcdC;
 	dut->trace(m_trace,5);
 	m_trace->open("waveform.vcd");
-
-	init_iram();
 
 	int n=2;
 	dut->reset=1;
@@ -96,9 +128,12 @@ int main(int argc,char** argv,char** env){
 
 	m_trace->close();
 
-	delete dut;
+	if(ebreak_dpi==1)
+	Log("NPC running over,because the dpi-c ebreak matters");
+	else
+	Log("NPC running over,because up to the max_sim_time");
 
-	free(iram);
+	delete dut;
 
 	exit(EXIT_SUCCESS);
 	
--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -4,26 +4,31 @@ module top(
     input [31:0] ist
             );
 
+wire addi,jal,jalr,auipc,lui;
 
-wire [31:0] pc_next ;
+assign addi = (ist[14:12]==3'b000) && (ist[6:0]==7'b0010011);   //I R(rd) = src1 + imm
+assign jalr = ((ist[14:12]==3'b000)&&(ist[6:0]==7'b1100111));   //I s->dnpc = (src1 + imm) & ~(word_t)1
+assign auipc = (ist[6:0]==7'b0010111);                          //U R(rd) = s -> pc + imm
+assign lui = (ist[6:0]==7'b0110111);                            //U R(rd) = imm
+assign jal = (ist[6:0]==7'b1101111);                            //J s->dnpc = s->pc + imm
 
-assign pc_next = pc + 4 ;
+wire inst_i,inst_u,inst_j;
 
-Reg #(32,32'h80000000) inst_pc (clk,reset,pc_next,pc,1'b1);
+assign inst_i=addi|jalr;
+assign inst_u=auipc|lui;
+assign inst_j=jal;
 
-wire [11:0] imm1;
+wire [11:0] imm;
 
 wire [4:0] rs1,rd;
 
-wire addi ; 
+assign imm =  inst_i ? {20'b0,ist[31:20]}   : 
+              inst_u ? {ist[31:12],12'b0}   :
+              inst_j ? {12'b0,ist[31],ist[7:0],ist[8],ist[18:9]}:'b0;
 
-assign addi = (ist[14:12]==3'b000) && (ist[6:0]==7'b0010011);
+assign rs1 = ist[19:15] ;
 
-assign imm1 = (addi==1'b1) ? ist[31:20] :'b0 ;
-
-assign rs1 = (addi==1'b1) ? ist[19:15] :'b0 ;
-
-assign rd = (addi==1'b1) ? ist[11:7] :'b0 ;
+assign rd = ist[11:7] ;
 
 wire [31:0] rf_wdata,rf_rdata;
 
@@ -35,12 +40,24 @@ assign rf_waddr = rd ;
 
 assign rf_raddr = rs1 ;
 
-assign rf_wen= 'b1 ;
+assign rf_wen= addi | auipc | lui ;
 
-assign rf_wdata = rf_rdata + {{20{imm1[11]}},imm1} ;
+assign rf_wdata =   addi ? rf_rdata + {{20{imm[11]}},imm}   :
+                    auipc? pc + imm                         :
+                    lui  ? imm                              :
+                    'b0;
+
 
 RegisterFile #(5,32) inst_RegisterFile 
 (clk,rf_wdata,rf_waddr,rf_wen,rf_rdata,rf_raddr);
+
+wire [31:0] pc_next ;
+
+assign pc_next  =   jalr ?  (rf_rdata + imm) & 0xfffffffe;
+                    jal  ?  pc+imm  :
+                    pc + 4          ;
+
+Reg #(32,32'h80000000) inst_pc (clk,reset,pc_next,pc,1'b1);
 
 
 dpi_c_ebreak inst_dpi_c_ebreak(ist);
