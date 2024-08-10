9da1dbccd1d719003e995bacfcd0850257672192
tracer-ysyx <tracer@ysyx.org>
2024-08-10 19:57:55 +0800

    > sim RTL ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:57:55 up  1:11,  1 user,  load average: 0.95, 1.04, 0.93

---
+++ npc/vsrc/RegisterFile.v
@@ -0,0 +1,24 @@
+module RegisterFile #(ADDR_WIDTH = 1, DATA_WIDTH = 1) (
+  input clk,
+  input [DATA_WIDTH-1:0] wdata,
+  input [ADDR_WIDTH-1:0] waddr,
+  input wen,
+  output [DATA_WIDTH-1:0] rdata,
+  input [ADDR_WIDTH-1:0] raddr
+);
+
+export "DPI-C" function read_reg;
+
+  reg [DATA_WIDTH-1:0] rf [2**ADDR_WIDTH-1:0];
+  always @(posedge clk) begin
+    if (wen) rf[waddr] <= wdata;
+  end
+
+  assign rdata = (raddr=='b0) ?     'b0     :
+                                    rf[raddr];
+  
+function [31:0] read_reg(input [4:0] reg_index);
+  return rf[reg_index];
+endfuction
+
+endmodule
