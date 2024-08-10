3a563b92b6117de9cc22b2568b2419c1a6987e41
tracer-ysyx <tracer@ysyx.org>
2024-08-10 15:54:08 +0800

    > sim RTL ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:54:08 up  1:06,  1 user,  load average: 1.67, 1.07, 0.98

--- npc/csrc/npc_init.c
+++ npc/csrc/npc_init.c
@@ -1,15 +1,6 @@
 #include "npc_sources.h"
 
 
-vluint64_t sim_time=0;
-
-int ebreak_dpi=0;
-
-void ebreak(){
-	ebreak_dpi=1;
-	return;
-}
-
 static uint8_t pmem[CONFIG_MSIZE] PG_ALIGN = {};
 
 uint8_t* guest_to_host(uint32_t paddr) { return pmem + paddr - CONFIG_MBASE; }
@@ -75,76 +66,3 @@ void load_img(){
   fclose(fp);
   return ;
 }
-
-
-int main(int argc,char** argv,char** env){
-	
-	int i=1;
-    do{
-		printf("argc[%d]=%s\n",i,argv[i-1]);
-    	i+=1;
-  	}while(i!=argc+1);
-
-	img_file=argv[1];
-
-	load_img();
-
-	//init_mem();
-
-	Vtop *dut = new Vtop;
-	Verilated::traceEverOn(true);
-	VerilatedVcdC *m_trace = new VerilatedVcdC;
-	dut->trace(m_trace,5);
-	m_trace->open("waveform.vcd");
-
-	int n=2;
-	dut->reset=1;
-	sim_time++;
-	m_trace->dump(sim_time);
-	while(n-->0){
-		dut->clk = 0;dut->eval();
-		m_trace->dump(sim_time);
-		sim_time++;
-		dut->clk = 1;dut->eval();
-		m_trace->dump(sim_time);
-		sim_time++;
-
-	}
-	dut->reset=0;
-	m_trace->dump(sim_time);
-	sim_time++;
-
-
-	int a,b;
-	while(sim_time<MAX_SIM_TIME&&ebreak_dpi==0){
-
-		dut->clk^=1;
-		
-		dut->ist = pmem_read(dut->pc,4);
-		printf("pc=%x\n",dut->pc);
-		
-		dut->eval();
-
-		m_trace->dump(sim_time);
-
-		sim_time++;
-
-	}
-
-	m_trace->close();
-
-	if(ebreak_dpi==1)
-	printf("NPC running over,because the dpi-c ebreak matters");
-	else
-	printf("NPC running over,because up to the max_sim_time");
-
-	delete dut;
-
-	exit(EXIT_SUCCESS);
-	
-	return 0;
-}
-
-
-
-
---
+++ npc/csrc/npc_sources.h
@@ -0,0 +1,28 @@
+#ifndef NPC_SOURCES_H
+#define NPC_SOURCES_H
+
+
+
+#include <stdlib.h>
+#include <iostream>
+#include <stdio.h>
+#include <assert.h>
+#include <assert.h>
+#include <verilated.h>
+#include <verilated_vcd_c.h>
+#include "Vtop.h"
+#include "svdpi.h"
+#include "Vtop__Dpi.h"
+
+#define MAX_SIM_TIME 200
+#define CONFIG_MBASE 0x80000000
+#define CONFIG_MSIZE 0x8000000
+#define CONFIG_PC_RESET_OFFSET 0x0
+#define PMEM_LEFT  ((uint32_t)CONFIG_MBASE)
+#define PMEM_RIGHT ((uint32_t)CONFIG_MBASE + CONFIG_MSIZE - 1)
+#define PMEM_LEFT  ((uint32_t)CONFIG_MBASE)
+#define RESET_VECTOR (PMEM_LEFT + CONFIG_PC_RESET_OFFSET)
+#define PG_ALIGN __attribute((aligned(4096)))
+
+
+#endif
