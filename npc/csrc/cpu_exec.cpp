9761f859effe9b94b5ce264c626845d7d293c034
tracer-ysyx <tracer@ysyx.org>
2024-08-10 23:46:31 +0800

    > sim RTL ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:46:31 up  2:11,  1 user,  load average: 1.92, 0.90, 0.64

--- npc/csrc/cpu_exec.cpp
+++ npc/csrc/cpu_exec.cpp
@@ -43,22 +43,13 @@ uint32_t previous_pc=0;
 void exec_cpu(uint32_t exec_time){
 	int curse = 0;
 
-	if(ebreak_dpi&&curse==0){ end_cpu();curse+=1;
-                    printf("\033[1;34m[%s,%d]Success!\nNPC running over,because the dpi-c ebreak matters\033[0m\r\n",__FILE__,__LINE__);
-					printf("\033[1;32m[%s,%d]HIT GOOD TRAP!\033[0m",__FILE__,__LINE__);
-					printf("at pc = %x\n",dut->pc);
-                    return ;}
-    else if(sim_time>=MAX_SIM_TIME&&curse==0){
-        end_cpu();curse+=1;
-        printf("\033[1;31m[%s:%d]Fail!\nNPC running over,because up to the max_sim_time\033[0m\n",__FILE__,__LINE__);
-        printf("\033[1;32m[%s,%d]HIT BAD TRAP!\033[0m\n",__FILE__,__LINE__);
-		return ;}
+	
 
-	else if( sim_time>=MAX_SIM_TIME || ebreak_dpi ){
+	if( (sim_time>=MAX_SIM_TIME || ebreak_dpi) && curse==1 ){
 		printf("the process of npc has been over,please restart NPC again\n");
 		return;
 	}
-	
+
     int exec_time_done ;
     for(exec_time_done=0;exec_time_done<exec_time;exec_time_done++){
         
@@ -78,7 +69,16 @@ void exec_cpu(uint32_t exec_time){
         if(ebreak_dpi||sim_time>=MAX_SIM_TIME)break;
 
     }
-    
+    if(ebreak_dpi&&curse==0){ end_cpu();curse+=1;
+                    printf("\033[1;34m[%s,%d]Success!\nNPC running over,because the dpi-c ebreak matters\033[0m\r\n",__FILE__,__LINE__);
+					printf("\033[1;32m[%s,%d]HIT GOOD TRAP!\033[0m",__FILE__,__LINE__);
+					printf("at pc = %x\n",dut->pc);
+                    return ;}
+    else if(sim_time>=MAX_SIM_TIME&&curse==0){
+        end_cpu();curse+=1;
+        printf("\033[1;31m[%s:%d]Fail!\nNPC running over,because up to the max_sim_time\033[0m\n",__FILE__,__LINE__);
+        printf("\033[1;32m[%s,%d]HIT BAD TRAP!\033[0m\n",__FILE__,__LINE__);
+		return ;}
     return;
 }
 uint32_t read_reg(int index){
