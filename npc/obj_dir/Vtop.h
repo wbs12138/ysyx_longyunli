7d835a7e440a9ad4ba6f24497dc4dcb39335277e
tracer-ysyx <tracer@ysyx.org>
2024-08-10 22:46:09 +0800

    > sim RTL ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:46:09 up  1:10,  1 user,  load average: 0.61, 0.38, 0.35

---
+++ npc/obj_dir/Vtop.h
@@ -0,0 +1,126 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: Verilator output: Primary model header
+//
+// This header should be included by all source files instantiating the design.
+// The class here is then constructed to instantiate the design.
+// See the Verilator manual for examples.
+
+#ifndef VERILATED_VTOP_H_
+#define VERILATED_VTOP_H_  // guard
+
+#include "verilated.h"
+#include "svdpi.h"
+
+class Vtop__Syms;
+class Vtop___024root;
+class VerilatedVcdC;
+
+// This class is the main interface to the Verilated model
+class alignas(VL_CACHE_LINE_BYTES) Vtop VL_NOT_FINAL : public VerilatedModel {
+  private:
+    // Symbol table holding complete model state (owned by this class)
+    Vtop__Syms* const vlSymsp;
+
+  public:
+
+    // CONSTEXPR CAPABILITIES
+    // Verilated with --trace?
+    static constexpr bool traceCapable = true;
+
+    // PORTS
+    // The application code writes and reads these signals to
+    // propagate new values into/out from the Verilated model.
+    VL_IN8(&clk,0,0);
+    VL_IN8(&reset,0,0);
+    VL_OUT(&pc,31,0);
+    VL_IN(&ist,31,0);
+    VL_OUT(&rf0,31,0);
+    VL_OUT(&rf1,31,0);
+    VL_OUT(&rf2,31,0);
+    VL_OUT(&rf3,31,0);
+    VL_OUT(&rf4,31,0);
+    VL_OUT(&rf5,31,0);
+    VL_OUT(&rf6,31,0);
+    VL_OUT(&rf7,31,0);
+    VL_OUT(&rf8,31,0);
+    VL_OUT(&rf9,31,0);
+    VL_OUT(&rf10,31,0);
+    VL_OUT(&rf11,31,0);
+    VL_OUT(&rf12,31,0);
+    VL_OUT(&rf13,31,0);
+    VL_OUT(&rf14,31,0);
+    VL_OUT(&rf15,31,0);
+    VL_OUT(&rf16,31,0);
+    VL_OUT(&rf17,31,0);
+    VL_OUT(&rf18,31,0);
+    VL_OUT(&rf19,31,0);
+    VL_OUT(&rf20,31,0);
+    VL_OUT(&rf21,31,0);
+    VL_OUT(&rf22,31,0);
+    VL_OUT(&rf23,31,0);
+    VL_OUT(&rf24,31,0);
+    VL_OUT(&rf25,31,0);
+    VL_OUT(&rf26,31,0);
+    VL_OUT(&rf27,31,0);
+    VL_OUT(&rf28,31,0);
+    VL_OUT(&rf29,31,0);
+    VL_OUT(&rf30,31,0);
+    VL_OUT(&rf31,31,0);
+
+    // CELLS
+    // Public to allow access to /* verilator public */ items.
+    // Otherwise the application code can consider these internals.
+
+    // Root instance pointer to allow access to model internals,
+    // including inlined /* verilator public_flat_* */ items.
+    Vtop___024root* const rootp;
+
+    // CONSTRUCTORS
+    /// Construct the model; called by application code
+    /// If contextp is null, then the model will use the default global context
+    /// If name is "", then makes a wrapper with a
+    /// single model invisible with respect to DPI scope names.
+    explicit Vtop(VerilatedContext* contextp, const char* name = "TOP");
+    explicit Vtop(const char* name = "TOP");
+    /// Destroy the model; called (often implicitly) by application code
+    virtual ~Vtop();
+  private:
+    VL_UNCOPYABLE(Vtop);  ///< Copying not allowed
+
+  public:
+    // API METHODS
+    /// Evaluate the model.  Application must call when inputs change.
+    void eval() { eval_step(); }
+    /// Evaluate when calling multiple units/models per time step.
+    void eval_step();
+    /// Evaluate at end of a timestep for tracing, when using eval_step().
+    /// Application must call after all eval() and before time changes.
+    void eval_end_step() {}
+    /// Simulation complete, run final blocks.  Application must call on completion.
+    void final();
+    /// Are there scheduled events to handle?
+    bool eventsPending();
+    /// Returns time at next time slot. Aborts if !eventsPending()
+    uint64_t nextTimeSlot();
+    /// Trace signals in the model; called by application code
+    void trace(VerilatedTraceBaseC* tfp, int levels, int options = 0) { contextp()->trace(tfp, levels, options); }
+    /// Retrieve name of this model instance (as passed to constructor).
+    const char* name() const;
+
+    // Abstract methods from VerilatedModel
+    const char* hierName() const override final;
+    const char* modelName() const override final;
+    unsigned threads() const override final;
+    /// Prepare for cloning the model at the process level (e.g. fork in Linux)
+    /// Release necessary resources. Called before cloning.
+    void prepareClone() const;
+    /// Re-init after cloning the model at the process level (e.g. fork in Linux)
+    /// Re-allocate necessary resources. Called after cloning.
+    void atClone() const;
+    std::unique_ptr<VerilatedTraceConfig> traceConfig() const override final;
+  private:
+    // Internal functions - trace registration
+    void traceBaseModel(VerilatedTraceBaseC* tfp, int levels, int options);
+};
+
+#endif  // guard
