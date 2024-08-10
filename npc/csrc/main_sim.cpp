137ec9f337901d6f3c2da5d17d733c7fc765a59b
tracer-ysyx <tracer@ysyx.org>
2024-08-10 16:36:49 +0800

    > sim RTL ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:36:49 up  1:48,  1 user,  load average: 1.04, 0.73, 0.64

--- npc/csrc/main_sim.cpp
+++ npc/csrc/main_sim.cpp
@@ -10,8 +10,6 @@ void ebreak(){
 	return;
 }
 
-
-
 int main(int argc,char** argv,char** env){
 	
 	int i=1;
@@ -19,6 +17,8 @@ int main(int argc,char** argv,char** env){
 		printf("argc[%d]=%s\n",i,argv[i-1]);
     	i+=1;
   	}while(i!=argc+1);
+
+	char 
 
 	char *img_file=NULL;
 	img_file=argv[1];
