fc27f781de196665f9b3aa6886ac6ff030908b84
tracer-ysyx <tracer@ysyx.org>
2024-08-10 23:05:39 +0800

    > sim RTL ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:05:39 up  1:30,  1 user,  load average: 0.86, 0.53, 0.42

--- npc/obj_dir/Vtop.mk
+++ npc/obj_dir/Vtop.mk
@@ -38,7 +38,7 @@ VM_USER_CFLAGS = \
 
 # User LDLIBS (from -LDFLAGS on Verilator command line)
 VM_USER_LDLIBS = \
-	readline \
+	/usr/include/readline \
 
 # User .cpp files (from .cpp's on Verilator command line)
 VM_USER_CLASSES = \
