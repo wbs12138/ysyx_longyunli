a69774c66e42a71ddb28781478ddf3938346c3f8
tracer-ysyx <tracer@ysyx.org>
2024-08-11 20:09:26 +0800

    > sim RTL ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:09:26 up  1:03,  1 user,  load average: 1.34, 0.90, 0.90

--- npc/csrc/npc_difftest.cpp
+++ npc/csrc/npc_difftest.cpp
@@ -63,7 +63,7 @@ printf("\n\n\n");
 
 }
 
-bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
+bool isa_difftest_checkregs(CPU_state *ref_r, uint32_t pc) {
   int reg_num = 32;
   for (int i = 0; i < reg_num; i++) {
     if (ref_r->gpr[i] != npc_cpu_state.gpr[i]) {
