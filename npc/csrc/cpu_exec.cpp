619195277616229170191c60a09398ccf7325ff1
tracer-ysyx <tracer@ysyx.org>
2024-08-11 20:51:12 +0800

    > sim RTL ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:51:12 up  1:45,  1 user,  load average: 0.94, 0.65, 0.66

--- npc/csrc/cpu_exec.cpp
+++ npc/csrc/cpu_exec.cpp
@@ -90,9 +90,9 @@ void exec_cpu(uint32_t exec_time){
 
 		m_trace->dump(sim_time);
 
-		
-        
-        //m_trace->dump(sim_time);
+        sim_time++;
+
+		m_trace->dump(sim_time);
 
         }
 		
@@ -127,7 +127,7 @@ void exec_cpu(uint32_t exec_time){
     else if(sim_time>=MAX_SIM_TIME&&curse==0){
         end_cpu();curse+=1;display_inst();
         printf("\033[1;31m[%s:%d]Fail!\nNPC running over,because of difftest or dead loop\033[0m\n",__FILE__,__LINE__);
-        printf("\033[1;32m[%s,%d]HIT BAD TRAP!\033[0m\n",__FILE__,__LINE__);
+        printf("\033[1;31m[%s,%d]HIT BAD TRAP!\033[0m\n",__FILE__,__LINE__);
 		return ;}
     return;
 }
