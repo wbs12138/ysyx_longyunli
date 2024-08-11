5380adcc878729e8722ba410e2ddbcbf0604e73f
tracer-ysyx <tracer@ysyx.org>
2024-08-11 12:30:42 +0800

    > sim RTL ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:30:42 up 13 min,  1 user,  load average: 1.22, 1.25, 0.89

--- npc/csrc/npc_ftrace.cpp
+++ npc/csrc/npc_ftrace.cpp
@@ -433,6 +433,8 @@ static void remove_tail_rec() {
 //int call_num=0;
 
 void trace_func_call(uint32_t pc, uint32_t target, bool is_tail) {
+	assert(symbol_tbl!=NULL);
+	
 	if (symbol_tbl == NULL) return;
 
 	++call_depth;
