8ff12bf2236db75fcd970c7edcb67829a9ec1001
tracer-ysyx <tracer@ysyx.org>
2024-08-14 17:33:27 +0800

    > sim RTL ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:33:27 up 11 min,  1 user,  load average: 1.25, 0.89, 0.54

--- npc/csrc/cpu_exec.cpp
+++ npc/csrc/cpu_exec.cpp
@@ -234,9 +234,9 @@ read_reg[0]=0;read_reg[1]= rf1;read_reg[2]= rf2;read_reg[3]= rf3;read_reg[4]= rf
 }
 
 int npc_pmem_read(int raddr) {
-    if(dut->mem_valid)assert(0);
+    if(dut->mem_valid){assert(0);
   return pmem_read(raddr&~0x3u,4);
-    else
+    }else
     return 0;
 }
 void npc_pmem_write(int waddr, int wdata, char wmask) {
