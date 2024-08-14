35f59e2dfe40e54a12bed7fead434340e74b600d
tracer-ysyx <tracer@ysyx.org>
2024-08-14 16:57:37 +0800

    > sim RTL ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:57:37 up  2:33,  1 user,  load average: 1.83, 1.43, 1.16

--- npc/csrc/main_sim.cpp
+++ npc/csrc/main_sim.cpp
@@ -30,8 +30,8 @@ int main(int argc,char** argv,char** env){
 
 	if(strcmp(argv[3],"is_batch_mode")==0)
 		sdb_main_loop(1);		
-	else{assert(0);
-		sdb_main_loop(0);
+	else{
+		sdb_main_loop(0);assert(0);
 	}
 
 	
