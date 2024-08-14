154f1963d262c8bd4a1583cc7c538fc58d193401
tracer-ysyx <tracer@ysyx.org>
2024-08-14 17:11:18 +0800

    > sim RTL ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:11:18 up  2:47,  1 user,  load average: 1.74, 1.64, 1.45

--- npc/csrc/cpu_exec.cpp
+++ npc/csrc/cpu_exec.cpp
@@ -58,7 +58,7 @@ void init_cpu(){
 	m_trace->dump(sim_time);
     
 	while(reset_clk_time-->0){
-		dut->clk = 0;assert(0);dut->eval();sim_time++;
+		dut->clk = 0;dut->ist=0;dut->eval();sim_time++;
 		m_trace->dump(sim_time);
 		
 		dut->clk = 1;dut->eval();sim_time++;
