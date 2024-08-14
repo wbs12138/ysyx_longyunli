7c5fad18d8de181a351254a0119c5593ced34d42
tracer-ysyx <tracer@ysyx.org>
2024-08-15 01:44:19 +0800

    > sim RTL ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:44:19 up  4:12,  1 user,  load average: 1.37, 1.15, 1.15

--- npc/csrc/cpu_exec.cpp
+++ npc/csrc/cpu_exec.cpp
@@ -112,7 +112,7 @@ void exec_cpu(uint32_t exec_time){
         trace_memory_r=0;
         trace_memory_w=0;
         //printf("0x80000328=%x\n",pmem_read(0x80000328,4));
-        //printf("0x800002ce=%x\n",pmem_read(0x800002ce,4));
+        printf("0x800002ce=%x\n",pmem_read(0x800004c1,4));
         update_state();
         error_happen = difftest_step(npc_cpu_state.pc);
         if(error_happen){
