462598c3e6473f4396963c64ff6302544ed58644
tracer-ysyx <tracer@ysyx.org>
2024-08-15 23:19:31 +0800

    > compile NEMU ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:19:31 up 30 min,  1 user,  load average: 0.32, 0.39, 0.52

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -6,7 +6,7 @@ void __am_timer_init() {
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
   uint32_t low=inl(RTC_ADDR);
-  uint32_t high=inl(RTC_ADDR+4);
+  uint32_t high=inl(RTC_ADDR+8);
   uptime->us = (uint64_t)low+(((uint64_t)high)<<32) ;
 }
 
