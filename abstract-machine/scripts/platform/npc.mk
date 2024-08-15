7d859aa8d6af6c8280144be7e88c876f2f60aa7f
tracer-ysyx <tracer@ysyx.org>
2024-08-15 11:43:57 +0800

    > sim RTL ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:43:57 up 3 min,  1 user,  load average: 2.10, 1.24, 0.53

--- abstract-machine/scripts/platform/npc.mk
+++ abstract-machine/scripts/platform/npc.mk
@@ -13,7 +13,7 @@ LDFLAGS   += -T $(AM_HOME)/scripts/linker.ld \
 						 --defsym=_pmem_start=0x80000000 --defsym=_entry_offset=0x0
 LDFLAGS   += --gc-sections -e _start
 CFLAGS += -DMAINARGS=\"$(mainargs)\"
-NPC_FLAGS = is_batch_mode
+NPC_FLAGS = is_not_batch_mode
 .PHONY: $(AM_HOME)/am/src/riscv/npc/trm.c
 
 image: $(IMAGE).elf
