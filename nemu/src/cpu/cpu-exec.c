aa3cba94717824efd2831eafd09d2129ecdb6acf
tracer-ysyx <tracer@ysyx.org>
2024-08-16 00:15:02 +0800

    > compile NEMU ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:15:02 up 22 min,  1 user,  load average: 0.03, 0.36, 0.47

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -39,12 +39,12 @@ void device_update();
 
 static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 #ifdef CONFIG_ITRACE_COND
-    //if (ITRACE_COND) { log_write("%s\n", _this->logbuf); }
-    if (ITRACE_COND) { log_write("%s\n", _this->logbuf);assert(0); }
+    if (ITRACE_COND) { log_write("%s\n", _this->logbuf); }
+    //if (ITRACE_COND) { log_write("%s\n", _this->logbuf);assert(0); }
 #endif
     if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
-    //IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
-    IFDEF(CONFIG_DIFFTEST, assert(0););
+    IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
+    //IFDEF(CONFIG_DIFFTEST, assert(0););
     
     
     for(int i = 0 ; i < NR_WP; i ++){
