e6bdfd497edb5a832e768785001196a57fa4a77a
tracer-ysyx <tracer@ysyx.org>
2024-08-16 20:17:57 +0800

    > compile NEMU ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:17:57 up 25 min,  1 user,  load average: 1.22, 1.48, 1.04

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -93,7 +93,7 @@ int printf(const char *fmt, ...) {
   char *start = buffer;
 
   for (; *fmt != '\0'; ++fmt) {
-    if (*fmt == '\033'){
+    if ((*fmt == '\033')||(*fmt == '\t')||(*fmt == '\n')){
       while(*start!='\0'){
       putch(*start);
       start++;
