5988e4b0490a66154308c6d71c4d9d185b4ed283
tracer-ysyx <tracer@ysyx.org>
2024-08-17 20:24:02 +0800

    > compile NEMU ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:24:02 up  1:05,  1 user,  load average: 0.78, 0.80, 0.80

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -94,7 +94,7 @@ int strncmp(const char *s1, const char *s2, size_t n) {
 
 }
 
-void *memset(void *s, int c, size_t n) {
+void *memset(void *s, int c, size_t n) {panic("no\n");
   if (s == NULL) {
     return s;
   }
