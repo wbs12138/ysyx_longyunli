c878807cc32163aaa0f05b3cdc2373aaf03c9f33
tracer-ysyx <tracer@ysyx.org>
2024-08-18 00:57:56 +0800

    > compile NEMU ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:57:56 up  5:39,  1 user,  load average: 0.46, 0.65, 0.61

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -59,7 +59,9 @@ word_t map_read(paddr_t addr, int len, IOMap *map) {
   paddr_t offset = addr - map->low;
   invoke_callback(map->callback, offset, len, false); // prepare data to read
   word_t ret = host_read(map->space + offset, len);
+  #ifdef CONFIG_ITRACE
   trace_dread(addr, len, map);
+  #endif
   return ret;
 }
 
@@ -68,6 +70,8 @@ void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
   check_bound(map, addr);
   paddr_t offset = addr - map->low;
   host_write(map->space + offset, len, data);
+  #ifdef CONFIG_ITRACE
   trace_dwrite(addr, len, data, map);
+  #endif
   invoke_callback(map->callback, offset, len, true);
 }
