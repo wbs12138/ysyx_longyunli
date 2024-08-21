3bd90e7abde77a9334e26cf1626db1a09d56474b
tracer-ysyx <tracer@ysyx.org>
2024-08-21 16:27:35 +0800

    > sim RTL ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:27:35 up  3:27,  1 user,  load average: 1.49, 1.04, 0.73

--- abstract-machine/am/src/riscv/npc/timer.c
+++ abstract-machine/am/src/riscv/npc/timer.c
@@ -1,10 +1,13 @@
 #include <am.h>
-
+#define DEVICE_BASE 0xa0000000
+#define NPC_RTC_ADDR        (DEVICE_BASE + 0x0000048)
 void __am_timer_init() {
 }
-
+static inline uint32_t inl(uintptr_t addr) { return *(volatile uint32_t *)addr; }
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uptime->us = 0;
+  uint32_t high=inl(NPC_RTC_ADDR+4);
+  uint32_t low=inl(NPC_RTC_ADDR);
+  uptime->us = (uint64_t)low+(((uint64_t)high)<<32) ;
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
