295d7e5becbce6d49df586967e6980b574cb208d
tracer-ysyx <tracer@ysyx.org>
2024-08-22 00:51:40 +0800

    > sim RTL ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:51:40 up  1:10,  1 user,  load average: 1.02, 0.99, 1.11

--- npc/csrc/npc_vga.cpp
+++ npc/csrc/npc_vga.cpp
@@ -1,8 +1,8 @@
 
 #include "npc_vga.h"
 
-#define SCREEN_W    100       //400 
-#define SCREEN_H    75       //300
+#define SCREEN_W    400 
+#define SCREEN_H    300
 
 static uint32_t screen_width() {
   return SCREEN_W;
