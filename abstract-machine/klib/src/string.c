ea6046a2285bcd1b5bdfd83ce239dc4c4b0cba7c
tracer-ysyx <tracer@ysyx.org>
2024-08-25 01:44:53 +0800

    > compile NEMU ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:44:53 up  2:57,  1 user,  load average: 1.40, 0.94, 0.79

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -139,7 +139,7 @@ void *memmove(void *dst, const void *src, size_t n) {assert(0);
 }
 
 
-void *memcpy(void *out, const void *in, size_t n) {assert(0);
+void *memcpy(void *out, const void *in, size_t n) {
   if (out == NULL || in == NULL || n == 0 || out == in) {
     return out;
   }
