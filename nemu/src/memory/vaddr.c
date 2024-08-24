2426c7250e0f4af50328011d3fe25a97be67f1ec
tracer-ysyx <tracer@ysyx.org>
2024-08-24 17:03:05 +0800

    > compile NEMU ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:03:05 up 14 min,  1 user,  load average: 1.34, 0.92, 0.61

--- nemu/src/memory/vaddr.c
+++ nemu/src/memory/vaddr.c
@@ -25,6 +25,7 @@ word_t vaddr_read(vaddr_t addr, int len) {
 }
 
 void vaddr_write(vaddr_t addr, int len, word_t data) {
-  if(addr==0x8001360c)printf("fffffffffffffffffffffffffffffffffffffffffffffffff,pc=%x\n",cpu.pc);
+  if(addr-0x8001360c>=-16 && addr-0x8001360c<=16)printf("fffffffffffffffffffffffffffffffffffffffffffffffff,pc=%x\n",cpu.pc);
+  
   paddr_write(addr, len, data);
 }
