343b7b6fe4fabc58f2806d078538383c877962af
tracer-ysyx <tracer@ysyx.org>
2024-08-24 20:59:04 +0800

    > compile NEMU ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:59:04 up 5 min,  1 user,  load average: 0.95, 1.31, 0.71

--- abstract-machine/am/include/arch/riscv.h
+++ abstract-machine/am/include/arch/riscv.h
@@ -9,8 +9,8 @@
 
 struct Context {
   // TODO: fix the order of these members to match trap.S
-   uintptr_t  gpr[NR_REGS], mcause, mstatus ,mepc;
- void *pdir;
+   void *pdir;uintptr_t  gpr[NR_REGS], mcause, mstatus ,mepc;
+ 
 };
 
 #ifdef __riscv_e
