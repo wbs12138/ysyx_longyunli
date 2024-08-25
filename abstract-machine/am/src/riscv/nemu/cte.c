f98d72e4f4281d6625f9331b7e7bb8e55278190c
tracer-ysyx <tracer@ysyx.org>
2024-08-25 16:28:08 +0800

    > compile NEMU ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:28:08 up  3:31,  1 user,  load average: 1.06, 2.32, 2.07

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -1,82 +1,22 @@
-// #include <am.h>
-// #include <riscv/riscv.h>
-// #include <klib.h>
-
-// static Context* (*user_handler)(Event, Context*) = NULL;
-
-// Context* __am_irq_handle(Context *c) {
-//   if (user_handler) {
-//     Event ev = {0};
-//     switch (c->mcause) {
-//       case 0x0b : ev.event = EVENT_YIELD; break;
-//       default   : ev.event = EVENT_ERROR; break;
-//     }
-
-//     c = user_handler(ev, c);
-//     assert(c != NULL);
-//   }
-
-//   return c;
-// }
-
-// extern void __am_asm_trap(void);
-
-// bool cte_init(Context*(*handler)(Event, Context*)) {
-//   // initialize exception entry
-//   asm volatile("csrw mtvec, %0" : : "r"(__am_asm_trap));
-
-//   // register event handler
-//   user_handler = handler;
-
-//   return true;
-// }
-
-// Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
-//   Context *this_context = (Context *)(kstack.end-sizeof(Context));
-//   this_context->mepc=(uintptr_t) entry - 4;
-//   this_context->mstatus=0x1800;
-//   this_context->gpr[10]=(uintptr_t) arg;
-//   return this_context;
-// }
-
-// void yield() {
-// #ifdef __riscv_e
-//   asm volatile("li a5, -1; ecall");
-// #else
-//   asm volatile("li a7, 11; ecall");
-// #endif
-// }
-
-// bool ienabled() {
-//   return false;
-// }
-
-// void iset(bool enable) {
-// }
 #include <am.h>
 #include <riscv/riscv.h>
 #include <klib.h>
 
-// @return: Context*
-// @parameter: Event, Context*
 static Context* (*user_handler)(Event, Context*) = NULL;
 
 Context* __am_irq_handle(Context *c) {
-  Context *next = NULL;
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
-      case 11: 
-        ev.event = EVENT_YIELD; 
-        c->mepc = c->mepc + 4;
-        break;
-      default: ev.event = EVENT_ERROR; break;
+      case 0x0b : ev.event = EVENT_YIELD; c->mepc+=4;break;
+      default   : ev.event = EVENT_ERROR; break;
     }
-    // printf("mcause: %d, mstatus: %d, mepc: %x\n", c->mcause, c->mstatus, c->mepc);
-    next = user_handler(ev, c);
+
+    c = user_handler(ev, c);
+    assert(c != NULL);
   }
-  assert(next != NULL);
-  return next;
+
+  return c;
 }
 
 extern void __am_asm_trap(void);
@@ -92,12 +32,11 @@ bool cte_init(Context*(*handler)(Event, Context*)) {
 }
 
 Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
-  Context *kctx = (Context *)(kstack.end - sizeof(Context));
-  memset(kctx, 0, sizeof(Context));
-  kctx->mepc = (uintptr_t)entry;
-  kctx->gpr[10] = (uintptr_t)arg;
-  *(Context **)kstack.start = kctx;
-  return kctx;
+  Context *this_context = (Context *)(kstack.end-sizeof(Context));
+  this_context->mepc=(uintptr_t) entry - 4;
+  this_context->mstatus=0x1800;
+  this_context->gpr[10]=(uintptr_t) arg;
+  return this_context;
 }
 
 void yield() {
@@ -114,3 +53,64 @@ bool ienabled() {
 
 void iset(bool enable) {
 }
+// #include <am.h>
+// #include <riscv/riscv.h>
+// #include <klib.h>
+
+// // @return: Context*
+// // @parameter: Event, Context*
+// static Context* (*user_handler)(Event, Context*) = NULL;
+
+// Context* __am_irq_handle(Context *c) {
+//   Context *next = NULL;
+//   if (user_handler) {
+//     Event ev = {0};
+//     switch (c->mcause) {
+//       case 11: 
+//         ev.event = EVENT_YIELD; 
+//         c->mepc = c->mepc + 4;
+//         break;
+//       default: ev.event = EVENT_ERROR; break;
+//     }
+//     // printf("mcause: %d, mstatus: %d, mepc: %x\n", c->mcause, c->mstatus, c->mepc);
+//     next = user_handler(ev, c);
+//   }
+//   assert(next != NULL);
+//   return next;
+// }
+
+// extern void __am_asm_trap(void);
+
+// bool cte_init(Context*(*handler)(Event, Context*)) {
+//   // initialize exception entry
+//   asm volatile("csrw mtvec, %0" : : "r"(__am_asm_trap));
+
+//   // register event handler
+//   user_handler = handler;
+
+//   return true;
+// }
+
+// Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
+//   Context *kctx = (Context *)(kstack.end - sizeof(Context));
+//   memset(kctx, 0, sizeof(Context));
+//   kctx->mepc = (uintptr_t)entry;
+//   kctx->gpr[10] = (uintptr_t)arg;
+//   *(Context **)kstack.start = kctx;
+//   return kctx;
+// }
+
+// void yield() {
+// #ifdef __riscv_e
+//   asm volatile("li a5, -1; ecall");
+// #else
+//   asm volatile("li a7, 11; ecall");
+// #endif
+// }
+
+// bool ienabled() {
+//   return false;
+// }
+
+// void iset(bool enable) {
+// }
