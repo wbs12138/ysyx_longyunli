0be5994338c0140dd5cd44970ed8aa4fcbe57964
tracer-ysyx <tracer@ysyx.org>
2024-08-25 17:15:46 +0800

    > compile NEMU ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:15:46 up  4:19,  1 user,  load average: 1.77, 1.13, 1.12

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -8,7 +8,7 @@ Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
-      case 0x0b : ev.event = EVENT_YIELD; c->mepc+=4;printf("in\n");break;
+      case 0x0b : ev.event = EVENT_YIELD; c->mepc+=4;break;
       default   : ev.event = EVENT_ERROR; break;
     }
 
@@ -53,64 +53,3 @@ bool ienabled() {
 
 void iset(bool enable) {
 }
-// #include <am.h>
-// #include <riscv/riscv.h>
-// #include <klib.h>
-
-// // @return: Context*
-// // @parameter: Event, Context*
-// static Context* (*user_handler)(Event, Context*) = NULL;
-
-// Context* __am_irq_handle(Context *c) {
-//   Context *next = NULL;
-//   if (user_handler) {
-//     Event ev = {0};
-//     switch (c->mcause) {
-//       case 11: 
-//         ev.event = EVENT_YIELD; 
-//         c->mepc = c->mepc + 4;
-//         break;
-//       default: ev.event = EVENT_ERROR; break;
-//     }
-//     // printf("mcause: %d, mstatus: %d, mepc: %x\n", c->mcause, c->mstatus, c->mepc);
-//     next = user_handler(ev, c);
-//   }
-//   assert(next != NULL);
-//   return next;
-// }
-
-// extern void __am_asm_trap(void);
-
-// bool cte_init(Context*(*handler)(Event, Context*)) {
-//   // initialize exception entry
-//   asm volatile("csrw mtvec, %0" : : "r"(__am_asm_trap));
-
-//   // register event handler
-//   user_handler = handler;
-
-//   return true;
-// }
-
-// Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
-//   Context *kctx = (Context *)(kstack.end - sizeof(Context));
-//   memset(kctx, 0, sizeof(Context));
-//   kctx->mepc = (uintptr_t)entry;
-//   kctx->gpr[10] = (uintptr_t)arg;
-//   *(Context **)kstack.start = kctx;
-//   return kctx;
-// }
-
-// void yield() {
-// #ifdef __riscv_e
-//   asm volatile("li a5, -1; ecall");
-// #else
-//   asm volatile("li a7, 11; ecall");
-// #endif
-// }
-
-// bool ienabled() {
-//   return false;
-// }
-
-// void iset(bool enable) {
-// }
