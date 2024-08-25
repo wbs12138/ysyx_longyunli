9fa10d28ce281ef4362a1c398d00e095902cfcdf
tracer-ysyx <tracer@ysyx.org>
2024-08-25 12:59:43 +0800

    > compile NEMU ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:59:43 up 3 min,  1 user,  load average: 1.68, 1.56, 0.68

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -49,7 +49,7 @@ static int itoa(int n, char *s, int base) {
 }
 
 
-int sprintf(char *out, const char *fmt, ...) {
+int sprintf(char *out, const char *fmt, ...) {assert(0);
   va_list pArgs;
   va_start(pArgs, fmt);
   char *start = out;
