9d155350a228c8b3edc5296b07a03dc8b0081e3c
tracer-ysyx <tracer@ysyx.org>
2024-08-26 01:50:49 +0800

    > compile NEMU ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:50:49 up  2:04,  1 user,  load average: 1.97, 1.14, 0.89

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -37,20 +37,6 @@ Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
   this_context->mepc=(uintptr_t) entry ;
   this_context->mstatus=0x1800;
   this_context->gpr[10]=(uintptr_t)arg;
-  arg+=sizeof(uintptr_t);
-  this_context->gpr[11]=(uintptr_t)arg;
-  arg+=sizeof(uintptr_t);
-  this_context->gpr[12]=(uintptr_t)arg;
-  arg+=sizeof(uintptr_t);
-  this_context->gpr[13]=(uintptr_t)arg;
-  arg+=sizeof(uintptr_t);
-  this_context->gpr[14]=(uintptr_t)arg;
-  arg+=sizeof(uintptr_t);
-  this_context->gpr[15]=(uintptr_t)arg;
-  arg+=sizeof(uintptr_t);
-  this_context->gpr[16]=(uintptr_t)arg;
-  arg+=sizeof(uintptr_t);
-  this_context->gpr[17]=(uintptr_t)arg;
 
   return this_context;
 }
