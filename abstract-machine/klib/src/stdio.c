66ff4b14b26ae9714be833e76fc444eecde6808a
tracer-ysyx <tracer@ysyx.org>
2024-08-26 14:20:14 +0800

    > compile NEMU ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:20:14 up 11 min,  1 user,  load average: 1.93, 2.12, 1.56

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -85,9 +85,9 @@ int sprintf(char *out, const char *fmt, ...) {
 }
 
 
-int printf(const char *fmt, ...) {return 0;
-  char info[4096];
-  for(int i=0;i<4096;i++)
+int printf(const char *fmt, ...) {
+  char info[65535];
+  for(int i=0;i<65535;i++)
   {
     info[i]='\0';
   }
