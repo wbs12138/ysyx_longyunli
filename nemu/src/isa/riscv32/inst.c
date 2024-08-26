2fb301b3841269717d23f7ece108e0fff1021b38
tracer-ysyx <tracer@ysyx.org>
2024-08-27 00:06:08 +0800

    > sim RTL ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:06:08 up  1:47,  1 user,  load average: 0.94, 0.56, 0.76

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -33,11 +33,10 @@ static vaddr_t *csr_register(word_t imm) {
   default: panic("Unknown csr");
   }
 }
-#ifdef CONFIG_RVE
-#define ECALL(dnpc) { bool success;dnpc = (isa_raise_intr(isa_reg_str2val("a7", &success), s->pc));}
-#else
+
+//#define ECALL(dnpc) { bool success;dnpc = (isa_raise_intr(isa_reg_str2val("a7", &success), s->pc));}
+
 #define ECALL(dnpc) { bool success;dnpc = (isa_raise_intr(isa_reg_str2val("a5", &success), s->pc));}
-#endif
 
 #define CSR(i) *csr_register(i)
 
