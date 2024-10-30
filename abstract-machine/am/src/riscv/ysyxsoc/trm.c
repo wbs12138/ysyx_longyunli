d708b37cce2e7543b6329a0f29af7bd2d5fdcca4
tracer-ysyx <tracer@ysyx.org>
2024-10-30 21:57:07 +0800

    > sim RTL ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:57:07 up 9 min,  1 user,  load average: 0.77, 0.97, 0.64

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -7,7 +7,7 @@ extern char _heap_start;
 int main(const char *args);
 
 extern char _pmem_start;
-#define PMEM_END  ((uintptr_t)&_heap_start + 0x0ffffff)
+#define PMEM_END  ((uintptr_t)&_heap_start + 0x0001fff)
 
 Area heap = RANGE(&_heap_start, PMEM_END);
 #ifndef MAINARGS
