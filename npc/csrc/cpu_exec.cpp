1ae0396232e1c6dcb7cefdcfe41b9f4d1690866a
tracer-ysyx <tracer@ysyx.org>
2024-10-30 11:00:03 +0800

    > sim RTL ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:00:03 up 12 min,  1 user,  load average: 2.60, 1.93, 1.23

--- npc/csrc/cpu_exec.cpp
+++ npc/csrc/cpu_exec.cpp
@@ -12,7 +12,7 @@
 #include <string.h>
 
 extern "C" void flash_read (int32_t addr, int32_t *data) { assert(0); }
-extern "C" void mrom_read  (int32_t addr, int32_t *data) { data = 0x00100073; }
+extern "C" void mrom_read  (int32_t addr, int32_t *data) { *data = 0x00100073; }
 
 
 int ebreak_dpi=0;
