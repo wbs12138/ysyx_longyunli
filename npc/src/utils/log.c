ea593967527cd18b81cdaceebe6e03ad842a6d41
tracer-ysyx <tracer@ysyx.org>
2024-10-31 20:48:26 +0800

    > sim RTL ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:48:26 up 47 min,  1 user,  load average: 0.90, 1.05, 1.00

--- npc/src/utils/log.c
+++ npc/src/utils/log.c
@@ -13,7 +13,7 @@
 * See the Mulan PSL v2 for more details.
 ***************************************************************************************/
 
-#include <common.h>
+#include </home/wangbaosen/ysyx/ysyx-workbench/npc/include/common.h>
 
 extern uint64_t g_nr_guest_inst;
 
