4aedd1657555403356f33b4ccfb6acee97a4c496
tracer-ysyx <tracer@ysyx.org>
2024-10-31 15:06:22 +0800

    > sim RTL ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:06:22 up 26 min,  1 user,  load average: 0.60, 0.94, 0.78

--- npc/include/common.h
+++ npc/include/common.h
@@ -21,7 +21,7 @@
 #include <stdbool.h>
 #include <string.h>
 
-#include <generated/autoconf.h>
+#include </home/wangbaosen/ysyx/ysyx-workbench/npc/include/generated/autoconf.h>
 #include <macro.h>
 
 #ifdef CONFIG_TARGET_AM
