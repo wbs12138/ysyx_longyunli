f055eedec7275b25f50fed8456ff7dc469b8b2ec
tracer-ysyx <tracer@ysyx.org>
2024-10-31 15:29:07 +0800

    > sim RTL ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:29:07 up 49 min,  1 user,  load average: 0.93, 0.91, 0.89

--- npc/include/cpu/ifetch.h
+++ npc/include/cpu/ifetch.h
@@ -15,7 +15,7 @@
 
 #ifndef __CPU_IFETCH_H__
 
-#include <memory/vaddr.h>
+#include </home/wangbaosen/ysyx/ysyx-workbench/npc/include/memory/vaddr.h>
 
 static inline uint32_t inst_fetch(vaddr_t *pc, int len) {
   uint32_t inst = vaddr_ifetch(*pc, len);
