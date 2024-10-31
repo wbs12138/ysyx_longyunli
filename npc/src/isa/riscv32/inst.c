551615b544decbf9755da10e49dd59e18f16c722
tracer-ysyx <tracer@ysyx.org>
2024-10-31 15:32:45 +0800

    > sim RTL ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:32:45 up 52 min,  1 user,  load average: 0.81, 1.10, 0.99

--- npc/src/isa/riscv32/inst.c
+++ npc/src/isa/riscv32/inst.c
@@ -14,9 +14,9 @@
 ***************************************************************************************/
 
 #include "local-include/reg.h"
-#include <cpu/cpu.h>
-#include <cpu/ifetch.h>
-#include <cpu/decode.h>
+#include </home/wangbaosen/ysyx/ysyx-workbench/npc/include/cpu/cpu.h>
+#include </home/wangbaosen/ysyx/ysyx-workbench/npc/include/cpu/cpu/ifetch.h>
+#include </home/wangbaosen/ysyx/ysyx-workbench/npc/include/cpu/cpu/decode.h>
 #include </home/wangbaosen/ysyx/ysyx-workbench/nemu/src/utils/itrace.h>
 
 #define R(i) gpr(i)
