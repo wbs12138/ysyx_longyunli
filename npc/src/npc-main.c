2851c37288aa3d378c4a877680cc75658d9da511
tracer-ysyx <tracer@ysyx.org>
2024-10-31 20:45:36 +0800

    > sim RTL ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:45:36 up 44 min,  1 user,  load average: 1.08, 1.01, 0.97

--- npc/src/npc-main.c
+++ npc/src/npc-main.c
@@ -15,10 +15,51 @@
 
 #include </home/wangbaosen/ysyx/ysyx-workbench/npc/include/common.h>
 
+#include </home/wangbaosen/ysyx/ysyx-workbench/npc/include/cpu/cpu.h>
+
+
 void init_monitor(int, char *[]);
 void am_init_monitor();
 void engine_start();
 int is_exit_status_bad();
+
+void init_verilator(int argc, char *argv[]) {
+
+  Verilated::commandArgs(argc,argv);  
+
+  Verilated::traceEverOn(true);
+	
+	dut->trace(m_trace,5);
+	m_trace->open("waveform.vcd");
+
+	int reset_clk_time=2;
+	dut->reset=1;
+	sim_time++;
+	m_trace->dump(sim_time);
+    
+	while(reset_clk_time-->0){
+		dut->clock = 0;
+        //dut->ist=0;
+        dut->eval();sim_time++;
+		m_trace->dump(sim_time);
+		
+		dut->clock = 1;dut->eval();sim_time++;
+		m_trace->dump(sim_time);
+		
+	}
+	dut->reset=0;
+    dut->eval();
+    //dut->ist = pmem_read(dut->pc,4);	
+    
+	dut->eval();
+    //pc_pre=dut->pc;
+    //update_state();
+    sim_time+=1;
+	m_trace->dump(sim_time);
+
+
+}
+
 
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
@@ -31,7 +72,7 @@ int main(int argc, char *argv[]) {
 
   init_monitor(argc, argv);
 
-  //init_verilator(argc, argv);
+  init_verilator(argc, argv);
 
   /* Start engine. */
   engine_start();
