118740c6fbf7d47754021cd70f3570f57ee24d8d
tracer-ysyx <tracer@ysyx.org>
2024-10-31 20:19:36 +0800

    > sim RTL ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:19:36 up 18 min,  1 user,  load average: 1.26, 1.01, 0.77

--- npc/vsrc/dpi_c_ecall.v
+++ npc/vsrc/dpi_c_ecall.v
@@ -6,12 +6,18 @@ import "DPI-C" function void npc_ecall(input int ecall, input int mret);
 
 wire ecall,mret;
 
+wire ecall_t,mret_t;
+
 assign ecall = (ist == 32'b00000000000000000000000001110011);
 assign mret  = (ist == 32'b00110000001000000000000001110011);
 
+assign ecall_t = {30'b0,ecall};
+
+assign mret_t = {30'b0,mret};
+
 always@(*)
 begin
-    npc_ecall(ecall,mret);
+    npc_ecall(ecall_t,mret_t);
 end
 
 endmodule
