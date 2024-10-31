d0eb2d041a91697e56553522f4dc47014c026d6c
tracer-ysyx <tracer@ysyx.org>
2024-10-31 14:59:13 +0800

    > sim RTL ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:59:13 up 19 min,  1 user,  load average: 1.92, 0.99, 0.59

--- npc/scripts/build.mk
+++ npc/scripts/build.mk
@@ -18,6 +18,7 @@ VSRCS += $(shell find $(abspath ../ysyxSoC/perip) -name "*.v")
 VSRCS += $(shell find $(abspath ../ysyxSoC/build) -name "*.v")
 UART_INCLUDE_DIR = ../ysyxSoC/perip/uart16550/rtl
 SPI_INCLUDE_DIR = ../ysyxSoC/perip/spi/rtl
+CSRC_INCLUDE_DIR = /home/wangbaosen/ysyx/ysyx-workbench/npc/include
 
 clean:
 	rm -rf $(BUILD_DIR)
