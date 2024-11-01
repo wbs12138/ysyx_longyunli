6e5991c7d7766409b5f83c4fbd866695e1ed01d6
tracer-ysyx <tracer@ysyx.org>
2024-11-01 16:12:00 +0800

    > sim RTL ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:12:00 up  1:00,  1 user,  load average: 0.79, 0.91, 0.99

--- npc/src/monitor/monitor.c
+++ npc/src/monitor/monitor.c
@@ -129,8 +129,9 @@ void init_monitor(int argc, char *argv[]) {
   long img_size = load_img();
 
   /* Initialize differential testing. */
-  init_difftest(diff_so_file, img_size, difftest_port);
-
+  #ifdef CONFIG_DIFFTEST
+    init_difftest(diff_so_file, img_size, difftest_port);
+  #endif
   /* Initialize the simple debugger. */
   init_sdb();
 
