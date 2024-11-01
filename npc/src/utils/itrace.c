2c04deb93dfce627ad70b060c5c06bb2bb662e70
tracer-ysyx <tracer@ysyx.org>
2024-11-01 14:22:27 +0800

    > sim RTL ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:22:27 up  1:28,  1 user,  load average: 0.91, 0.68, 0.51

--- npc/src/utils/itrace.c
+++ npc/src/utils/itrace.c
@@ -1,8 +1,3 @@
-#include </home/wangbaosen/ysyx/ysyx-workbench/npc/src/utils/disasm.h>
-#ifdef concat
-#undef concat
-#endif
-
 #include </home/wangbaosen/ysyx/ysyx-workbench/npc/include/common.h>
 #include <fcntl.h>
 #include <unistd.h>
--- npc/src/utils/itrace.h
+++ npc/src/utils/itrace.h
@@ -2,6 +2,11 @@
 #ifndef __ITRACE_H__
 #define __ITRACE_H__
 
+#include </home/wangbaosen/ysyx/ysyx-workbench/npc/src/utils/disasm.h>
+#ifdef concat
+#undef concat
+#endif
+
 
 #include </home/wangbaosen/ysyx/ysyx-workbench/npc/include/common.h>
 #include <fcntl.h>
