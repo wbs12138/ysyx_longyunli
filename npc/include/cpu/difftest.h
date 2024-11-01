ff8b6f3230b61763dd70d0a90c3725027db4eb61
tracer-ysyx <tracer@ysyx.org>
2024-11-01 16:11:21 +0800

    > sim RTL ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:11:21 up  1:00,  1 user,  load average: 0.96, 0.96, 1.01

--- npc/include/cpu/difftest.h
+++ npc/include/cpu/difftest.h
@@ -20,6 +20,7 @@
 #include </home/wangbaosen/ysyx/ysyx-workbench/npc/include/difftest-def.h>
 
 #ifdef CONFIG_DIFFTEST
+void init_difftest(char *ref_so_file, long img_size, int port);
 void difftest_skip_ref();
 void difftest_skip_dut(int nr_ref, int nr_dut);
 void difftest_set_patch(void (*fn)(void *arg), void *arg);
@@ -27,6 +28,7 @@ void difftest_step(vaddr_t pc, vaddr_t npc);
 void difftest_detach();
 void difftest_attach();
 #else
+static inline void init_difftest(char *ref_so_file, long img_size, int port);
 static inline void difftest_skip_ref() {}
 static inline void difftest_skip_dut(int nr_ref, int nr_dut) {}
 static inline void difftest_set_patch(void (*fn)(void *arg), void *arg) {}
