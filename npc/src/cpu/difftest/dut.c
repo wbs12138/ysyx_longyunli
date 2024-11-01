8aff8181e2a0256b4f740a061798c7a26cc16fec
tracer-ysyx <tracer@ysyx.org>
2024-11-01 16:36:13 +0800

    > sim RTL ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:36:13 up  1:24,  1 user,  load average: 1.48, 1.46, 1.16

--- npc/src/cpu/difftest/dut.c
+++ npc/src/cpu/difftest/dut.c
@@ -63,6 +63,8 @@ void difftest_skip_dut(int nr_ref, int nr_dut) {
 void init_difftest(char *ref_so_file, long img_size, int port) {
   assert(ref_so_file != NULL);
 
+  printf("so_file is %s\n",ref_so_file);
+
   void *handle;
   handle = dlopen(ref_so_file, RTLD_LAZY);
   assert(handle);
