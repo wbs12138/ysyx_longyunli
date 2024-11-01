2cdfadf3f52719387862214bb99bc212cca56b18
tracer-ysyx <tracer@ysyx.org>
2024-11-01 14:30:20 +0800

    > sim RTL ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:30:20 up  1:36,  1 user,  load average: 0.41, 0.51, 0.51

--- npc/src/cpu/cpu-exec.c
+++ npc/src/cpu/cpu-exec.c
@@ -12,6 +12,13 @@
 *
 * See the Mulan PSL v2 for more details.
 ***************************************************************************************/
+
+#include </home/wangbaosen/ysyx/ysyx-workbench/npc/src/utils/disasm.h>
+#ifdef concat
+#undef concat
+#endif
+
+
 #include <../include/cpu/cpu.h>
 #include </home/wangbaosen/ysyx/ysyx-workbench/npc/include/cpu/decode.h>
 #include </home/wangbaosen/ysyx/ysyx-workbench/npc/include/cpu/difftest.h>
