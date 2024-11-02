cc2539d4338a8c713c1226a094c7298711e493ec
tracer-ysyx <tracer@ysyx.org>
2024-11-02 15:39:41 +0800

    > sim RTL ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:39:41 up  3:40,  1 user,  load average: 0.32, 0.75, 0.90

--- npc/include/generated/autoconf.h
+++ npc/include/generated/autoconf.h
@@ -4,15 +4,16 @@
  * NEMU Configuration Menu
  *
  */
-#define CONFIG_DIFFTEST_REF_NAME "none"
+#define CONFIG_DIFFTEST_REF_NAME "spike"
 #define CONFIG_ENGINE "interpreter"
 #define CONFIG_PC_RESET_OFFSET 0x0
+#define CONFIG_DIFFTEST_REF_SPIKE 1
 #define CONFIG_TARGET_NATIVE_ELF 1
 #define CONFIG_MSIZE 0xfff
 #define CONFIG_CC_O2 1
 #define CONFIG_MODE_SYSTEM 1
-#define CONFIG_MEM_RANDOM 1
 #define CONFIG_ITRACE 1
+#define CONFIG_DIFFTEST 1
 #define CONFIG_ISA_riscv 1
 #define CONFIG_TRACE_END 10000
 #define CONFIG_MTRACE_COND "true"
@@ -24,7 +25,7 @@
 #define CONFIG_RT_CHECK 1
 #define CONFIG_ITRACE_COND "true"
 #define CONFIG_CC "gcc"
-#define CONFIG_DIFFTEST_REF_PATH "none"
+#define CONFIG_DIFFTEST_REF_PATH "tools/spike-diff"
 #define CONFIG_TRACE_START 0
 #define CONFIG_CC_GCC 1
 #define CONFIG_TRACE 1
