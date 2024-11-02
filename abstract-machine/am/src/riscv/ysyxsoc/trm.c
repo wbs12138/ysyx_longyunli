04876a59cccbaf8f0759f194579cc6aaca3a2a61
tracer-ysyx <tracer@ysyx.org>
2024-11-02 15:24:42 +0800

    > sim RTL ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:24:42 up  3:25,  1 user,  load average: 1.67, 1.22, 0.92

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -43,10 +43,10 @@ void halt(int code) {
 
 void _trm_init() {
   if (data_start != data_load_start){
+    halt(0);
     memcpy(data_start, data_load_start, (size_t) data_size);
   }
-  else 
-    halt(0);
+
   int ret = main(mainargs);
   halt(ret);
 }
