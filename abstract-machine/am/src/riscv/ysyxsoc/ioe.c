021d2a50950e02b6b3427123eeb16ca7b9e812f8
tracer-ysyx <tracer@ysyx.org>
2024-11-10 20:47:35 +0800

    > sim RTL ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:47:35 up 43 min,  1 user,  load average: 1.20, 1.05, 1.01

--- abstract-machine/am/src/riscv/ysyxsoc/ioe.c
+++ abstract-machine/am/src/riscv/ysyxsoc/ioe.c
@@ -11,28 +11,11 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *);
 void __am_timer_uptime(AM_TIMER_UPTIME_T *);
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *);
 
-static void __am_timer_config(AM_TIMER_CONFIG_T *cfg) { ; }
-static void __am_input_config(AM_INPUT_CONFIG_T *cfg) { ;  }
-static void __am_uart_config(AM_INPUT_CONFIG_T *cfg) { ;  }
-
 static inline uint32_t inl(uintptr_t addr) { return *(volatile uint32_t *)addr; }
 
 static inline void outl(uintptr_t addr, uint32_t data) { *(volatile uint32_t *)addr = data; }
 
 typedef void (*handler_t)(void *buf);
-static void *lut[128] = {
-  [AM_TIMER_CONFIG] = __am_timer_config,
-  [AM_TIMER_RTC   ] = __am_timer_rtc,
-  [AM_TIMER_UPTIME] = __am_timer_uptime,
-  [AM_INPUT_CONFIG] = __am_input_config,
-  [AM_INPUT_KEYBRD] = __am_input_keybrd,
-  [AM_GPU_CONFIG  ] = __am_gpu_config,
-  [AM_GPU_FBDRAW  ] = __am_gpu_fbdraw,
-  [AM_GPU_STATUS  ] = __am_gpu_status,
-  [AM_UART_CONFIG]  = __am_uart_config,
-};
-
-static void fail(void *buf) { panic("access nonexist register"); }
 
 bool ioe_init() {
 
--- abstract-machine/scripts/platform/ysyxsoc.mk
+++ abstract-machine/scripts/platform/ysyxsoc.mk
@@ -1,5 +1,12 @@
 AM_SRCS := riscv/ysyxsoc/start.S \
-           riscv/ysyxsoc/trm.c
+           riscv/ysyxsoc/trm.c \
+           riscv/ysyxsoc/ioe.c \
+           riscv/ysyxsoc/timer.c \
+           riscv/ysyxsoc/input.c \
+           riscv/ysyxsoc/cte.c \
+           riscv/ysyxsoc/trap.S \
+           platform/dummy/vme.c \
+           platform/dummy/mpe.c
 
 CFLAGS    += -fdata-sections -ffunction-sections
 LDFLAGS   += -T $(AM_HOME)/scripts/linker_ysyxsoc.ld \
