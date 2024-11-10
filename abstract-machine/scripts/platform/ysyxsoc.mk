554f4557c55b0d363e23af52d361e930d245a519
tracer-ysyx <tracer@ysyx.org>
2024-11-10 13:53:37 +0800

    > sim RTL ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:53:37 up 21 min,  1 user,  load average: 1.12, 1.05, 1.04

--- abstract-machine/scripts/platform/ysyxsoc.mk
+++ abstract-machine/scripts/platform/ysyxsoc.mk
@@ -6,7 +6,7 @@ LDFLAGS   += -T $(AM_HOME)/scripts/linker_ysyxsoc.ld \
 						 --defsym=_pmem_start=0x30000000 --defsym=_entry_offset=0x0 --defsym=_heap_end=0x84000000
 LDFLAGS   += --gc-sections -e _start
 CFLAGS += -DMAINARGS=\"$(mainargs)\"
-YSYXSOC_FLAGS = -l $(shell dirname $(IMAGE).elf)/npc-log.txt -e $(IMAGE).elf
+YSYXSOC_FLAGS = -l $(shell dirname $(IMAGE).elf)/npc-log.txt -b -e $(IMAGE).elf
 .PHONY: $(AM_HOME)/am/src/riscv/ysyxsoc/trm.c
 
 image: $(IMAGE).elf
