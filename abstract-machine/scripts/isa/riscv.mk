f55d370d41d2176f9c9f165ea0470faf8f00c599
tracer-ysyx <tracer@ysyx.org>
2024-11-10 21:16:11 +0800

    > sim RTL ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:16:11 up  1:11,  1 user,  load average: 0.73, 0.88, 1.01

--- abstract-machine/scripts/isa/riscv.mk
+++ abstract-machine/scripts/isa/riscv.mk
@@ -1,7 +1,7 @@
 CROSS_COMPILE := riscv64-linux-gnu-
 COMMON_CFLAGS := -fno-pic -march=rv64g -mcmodel=medany -mstrict-align
 CFLAGS        += $(COMMON_CFLAGS) -static
-ASFLAGS       += $(COMMON_CFLAGS) -O0
+ASFLAGS       += $(COMMON_CFLAGS) -O1
 LDFLAGS       += -melf64lriscv
 
 # overwrite ARCH_H defined in $(AM_HOME)/Makefile
