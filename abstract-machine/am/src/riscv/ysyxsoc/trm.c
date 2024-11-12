92d30686bf629029da51ad91695473374bc8b7c8
tracer-ysyx <tracer@ysyx.org>
2024-11-12 15:48:52 +0800

    > sim RTL ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:48:52 up  2:35,  1 user,  load average: 2.16, 2.16, 1.88

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -101,6 +101,8 @@ void _trm_init() {
   printf("ysyx, whose ascii is %x,welcome!\n",ysyx);
   printf("wangbaosen, whose number is %x,welcome!\n",ysyx_number);
 
+  printf("heap_start is %x\n",(uintptr_t)&_heap_start);
+
   int ret = main(mainargs);
   halt(ret);
 }
