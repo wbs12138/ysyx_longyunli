076fd54608ef6d8df759e9fe964b52345b7f8da3
tracer-ysyx <tracer@ysyx.org>
2024-11-24 16:19:52 +0800

    > sim RTL ysyx_22040000 WANGBAOSEN Linux wangbaosen-virtual-machine 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:19:52 up  1:12,  1 user,  load average: 1.25, 1.04, 0.99

--- npc/scripts/native.mk
+++ npc/scripts/native.mk
@@ -31,7 +31,6 @@ NVBOARD_VERILATOR_CFLAGS += -MMD --build -cc  \
 
 NVBOARD_BUILD_DIR = ./nvboard_build
 $(shell mkdir -p $(NVBOARD_BUILD_DIR))
-NVBOARD_OBJ_DIR = $(NVBOARD_BUILD_DIR)/obj_dir
 
 # constraint file
 SRC_AUTO_BIND = /home/wangbaosen/ysyx/ysyx-workbench/npc/nvboard_build/auto_bind.cpp
@@ -45,8 +44,7 @@ include $(NVBOARD_HOME)/scripts/nvboard.mk
 
 run:
 	$(call git_commit, "sim RTL") # DO NOT REMOVE THIS LINE!!!
-	@rm -rf $(NVBOARD_OBJ_DIR)
-	verilator $(VSRCS) $(SRCS) $(NVBOARD_ARCHIVE) $(NVBOARD_VERILATOR_CFLAGS) --Mdir $(NVBOARD_OBJ_DIR) --cc --exe --build -j --trace --autoflush --top-module $(TOPNAME) --timescale "1ns/1ns" --no-timing +incdir+$(UART_INCLUDE_DIR) +incdir+$(SPI_INCLUDE_DIR) +incdir+$(CSRC_INCLUDE_DIR) -LDFLAGS '-lreadline -lSDL2 $(shell llvm-config --libs)' $(addprefix -CFLAGS , $(NVBOARD_CXXFLAGS)) $(addprefix -LDFLAGS , $(NVBOARD_LDFLAGS))
+	verilator $(VSRCS) $(SRCS) $(NVBOARD_ARCHIVE) $(NVBOARD_VERILATOR_CFLAGS) --cc --exe --build -j --trace --autoflush --top-module $(TOPNAME) --timescale "1ns/1ns" --no-timing +incdir+$(UART_INCLUDE_DIR) +incdir+$(SPI_INCLUDE_DIR) +incdir+$(CSRC_INCLUDE_DIR) -LDFLAGS '-lreadline -lSDL2 $(shell llvm-config --libs)' $(addprefix -CFLAGS , $(NVBOARD_CXXFLAGS)) $(addprefix -LDFLAGS , $(NVBOARD_LDFLAGS))
 	obj_dir/V$(TOPNAME) $(ARGS) $(IMG)
 
 clean-tools = $(dir $(shell find ./tools -maxdepth 2 -mindepth 2 -name "Makefile"))
